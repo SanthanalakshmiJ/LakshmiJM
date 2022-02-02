0fb0cb7470284d7224455526767219078427110f
SanthanalakshmiJ <santhanamarts@gmail.com>
2022-02-02 17:32:41 +0530

    Laksh update

---
+++ src/test/java/org/gitt/gitSample.java
@@ -0,0 +1,35 @@
+package org.gitt;
+
+public class gitSample {
+	
+	public static void main(String[] args) {
+		
+
+		System.out.println("Santhanalakshmi J");
+		System.out.println("Magesh Kumar M");
+		System.out.println("Ayyappan J");
+		System.out.println("Amutha J");
+		System.out.println("Jayaraman J");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
